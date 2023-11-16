043f8dd255c5cf67e13f78b19c30b57a473108a0
giuliomarinelli <giulio.marinelli@icloud.com>
2023-11-16 10:35:08 +0100

    push

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const key = readCookie('SpotifyBearer')
+console.log(key);
+
+function writeCookie(nomecookie, testo) {
+    let now = new Date();//Date crea un oggetto data contenente data ed ora attuali
+    //now.getMonth()//il mese attuale
+    now.setHours(now.getHours() + 1);//Alla data attuale aggiungo un'ora
+
+    let scadenza = "expires=" + now.toUTCString();//converto la data nel formato utc, richiesto per il corretto funzionamento del cookie. esempio: Wed, 14 Jun 2017 07:00:00 GMT
+
+    document.cookie = `${nomecookie}=${testo};${scadenza}`;
+}
+
+function readCookie(cookieDaLeggere) {
+    let allCookies = document.cookie;
+
+
+    // Dividiamo i cookies in un array di elementi chiave/valore
+    let arr = allCookies.split('; ');//["cookie_sessione=test", "nome=Michele", "mostra_popup=no"]
+
+    let chiave, valore;
+
+
+    let res = '';
+    for (let i = 0; i < arr.length; i++) {
+
+        chiave = arr[i].split('=')[0];//"mostra_popup"
+        valore = arr[i].split('=')[1];//"no"
+        if (cookieDaLeggere == chiave) {
+            res = valore;
+        }
+
+    }
+    return res;
+}
+
--- js/auth2.js
+++ js/auth2.js
@@ -7,7 +7,7 @@ function queryString(params) {
 }
 
 // URL di redirect e credenziali dell'app Spotify
-var redirect_uri = 'http://127.0.0.1:5500/homepage.html';
+var redirect_uri = 'http://127.0.0.1:5500/app.html';
 var client_id = 'a08665dce25b4dd4b88d1d5a503b023e';
 var client_secret = defaultExport;
 
@@ -30,7 +30,7 @@ function handleCallback() {
         })
     };
 
-    let options;
+    //let options;
     // Effettua la richiesta di token
     fetch('https://accounts.spotify.com/api/token', authOptions)
         .then(response => response.json())
@@ -46,13 +46,13 @@ function handleCallback() {
                 }
 
 
-                const res = await fetch(`https://api.spotify.com/v1/me`, options)
-                console.log(await res.json())
-                const res1 = await fetch('https://api.spotify.com/v1/me/player', options)
-               
+                //const res = await fetch(`https://api.spotify.com/v1/me`, options)
+                //console.log(await res.json())
+                //const res1 = await fetch('https://api.spotify.com/v1/me/player?market=IT&additional_types=track', options)
+                //console.log(res1)
             }
-            getProfile()
-            
+            console.log('key', data.access_token);
+            writeCookie('SpotifyBearer', data.access_token);
         })
         .catch(error => {
             // Gestisci eventuali errori nella richiesta
@@ -63,4 +63,38 @@ function handleCallback() {
 
 
 // Chiamare la funzione di gestione del callback al caricamento della pagina
-handleCallback();
+if (!readCookie('SpotifyBearer')) handleCallback();
+
+function writeCookie(nomecookie, testo) {
+    let now = new Date();//Date crea un oggetto data contenente data ed ora attuali
+    //now.getMonth()//il mese attuale
+    now.setHours(now.getHours() + 1);//Alla data attuale aggiungo un'ora
+
+    let scadenza = "expires=" + now.toUTCString();//converto la data nel formato utc, richiesto per il corretto funzionamento del cookie. esempio: Wed, 14 Jun 2017 07:00:00 GMT
+
+    document.cookie = `${nomecookie}=${testo};${scadenza}`;
+}
+
+function readCookie(cookieDaLeggere) {
+    let allCookies = document.cookie;
+
+
+    // Dividiamo i cookies in un array di elementi chiave/valore
+    let arr = allCookies.split('; ');//["cookie_sessione=test", "nome=Michele", "mostra_popup=no"]
+
+    let chiave, valore;
+
+
+    let res = '';
+    for (let i = 0; i < arr.length; i++) {
+
+        chiave = arr[i].split('=')[0];//"mostra_popup"
+        valore = arr[i].split('=')[1];//"no"
+        if (cookieDaLeggere == chiave) {
+            res = valore;
+        }
+
+    }
+    return res;
+}
+
